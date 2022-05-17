92873567ad8455f7e4e04be0e85bf53b738efe9a
Maver1k92 <70327932+Maver1k92@users.noreply.github.com>
2022-05-17 21:23:20 +0300

    Update initDB.sql

--- initDB.sql
+++ initDB.sql
@@ -3,21 +3,21 @@ create database it_marketplace;
 create table customers (
 id int not null auto_increment,
 customer_name varchar(50) not null,
-companies_id int not null,
+companies_country varchar(100) not null,
 primary key(id)
 );
 
 create table companies(
 id int not null auto_increment,
 company_name varchar(150) not null,
-projects_id int not null,
+company_country varchar(150) not null,
 primary key(id)
 );
 
 create table projects(
 id int not null auto_increment,
 project_name varchar(200) not null,
-skills_id int not null,
+project_description varchar(200),
 primary key(id)
 );
 
@@ -32,67 +32,40 @@ create table developers(
 id int not null auto_increment,
 first_name varchar(100) not null,
 last_name varchar(100) not null,
-dev_skill_id int not null,
-dev_project_id int not null,
-dev_company_id int not null,
 primary key(id)
 );
 
-ALTER TABLE `it_marketplace`.`companies` 
-ADD INDEX `project_id_idx` (`projects_id` ASC) VISIBLE;
+create table developers_market_info(
+dev_id int not null,
+skill_id int not null,
+project_id int,
+company_id int
+);
+
+ALTER TABLE `it_marketplace`.`developers_market_info` 
+ADD INDEX `dev_id_idx` (`dev_id` ASC) VISIBLE,
+ADD INDEX `skill_id_idx` (`skill_id` ASC) VISIBLE,
+ADD INDEX `project_id_idx` (`project_id` ASC) VISIBLE,
+ADD INDEX `company_id_idx` (`company_id` ASC) VISIBLE;
 ;
-ALTER TABLE `it_marketplace`.`companies` 
+ALTER TABLE `it_marketplace`.`developers_market_info` 
+ADD CONSTRAINT `dev_id`
+  FOREIGN KEY (`dev_id`)
+  REFERENCES `it_marketplace`.`developers` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `skill_id`
+  FOREIGN KEY (`skill_id`)
+  REFERENCES `it_marketplace`.`skills` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
 ADD CONSTRAINT `project_id`
-  FOREIGN KEY (`projects_id`)
-  REFERENCES `it_marketplace`.`projects` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-  
-  ALTER TABLE `it_marketplace`.`customers` 
-ADD INDEX `companies_id_idx` (`companies_id` ASC) VISIBLE;
-;
-ALTER TABLE `it_marketplace`.`customers` 
-ADD CONSTRAINT `companies_id`
-  FOREIGN KEY (`companies_id`)
-  REFERENCES `it_marketplace`.`companies` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-  
-  ALTER TABLE `it_marketplace`.`projects` 
-ADD INDEX `skills_id_idx` (`skills_id` ASC) VISIBLE;
-;
-ALTER TABLE `it_marketplace`.`projects` 
-ADD CONSTRAINT `skills_id`
-  FOREIGN KEY (`skills_id`)
-  REFERENCES `it_marketplace`.`skills` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-  
-  ALTER TABLE `it_marketplace`.`developers` 
-ADD INDEX `dev_skills_id_idx` (`dev_skill_id` ASC) VISIBLE,
-ADD INDEX `dev_project_id_idx` (`dev_project_id` ASC) VISIBLE,
-ADD INDEX `dev_company_id_idx` (`dev_company_id` ASC) VISIBLE;
-;
-ALTER TABLE `it_marketplace`.`developers` 
-ADD CONSTRAINT `dev_skill_id`
-  FOREIGN KEY (`dev_skill_id`)
-  REFERENCES `it_marketplace`.`skills` (`id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION,
-ADD CONSTRAINT `dev_project_id`
-  FOREIGN KEY (`dev_project_id`)
+  FOREIGN KEY (`project_id`)
   REFERENCES `it_marketplace`.`projects` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION,
-ADD CONSTRAINT `dev_company_id`
-  FOREIGN KEY (`dev_company_id`)
+ADD CONSTRAINT `company_id`
+  FOREIGN KEY (`company_id`)
   REFERENCES `it_marketplace`.`companies` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
-
-  
-
-  
-
-
-
