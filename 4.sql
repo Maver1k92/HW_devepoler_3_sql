ec4b834cbacad07c051ee9c6ec03748d7a61a071
Maver1k92 <70327932+Maver1k92@users.noreply.github.com>
2022-05-20 13:37:20 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,10 +1,2 @@
-ALTER TABLE `it_marketplace`.`projects` 
-ADD COLUMN `cost` INT NULL AFTER `project_description`;
-
-UPDATE `it_marketplace`.`projects` SET `cost` = '100' WHERE (`id` = '1');
-UPDATE `it_marketplace`.`projects` SET `cost` = '500' WHERE (`id` = '2');
-UPDATE `it_marketplace`.`projects` SET `cost` = '600' WHERE (`id` = '3');
-UPDATE `it_marketplace`.`projects` SET `cost` = '1000' WHERE (`id` = '4');
-UPDATE `it_marketplace`.`projects` SET `cost` = '200' WHERE (`id` = '5');
-UPDATE `it_marketplace`.`projects` SET `cost` = '700' WHERE (`id` = '6');
-UPDATE `it_marketplace`.`projects` SET `cost` = '800' WHERE (`id` = '7');
+ALTER TABLE projects
+ADD COLUMN cost bigint not NULL;
