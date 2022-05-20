1d2b6d0405b56800babed2b47ee6abee8fdb0fc5
Maver1k92 <70327932+Maver1k92@users.noreply.github.com>
2022-05-20 13:18:47 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,11 +1,15 @@
-ALTER TABLE `it_marketplace`.`developers` 
-ADD COLUMN `salary` INT NULL AFTER `last_name`;
-
-UPDATE `it_marketplace`.`developers` SET `salary` = '5500' WHERE (`id` = '1');
-UPDATE `it_marketplace`.`developers` SET `salary` = '5500' WHERE (`id` = '2');
-UPDATE `it_marketplace`.`developers` SET `salary` = '1500' WHERE (`id` = '3');
-UPDATE `it_marketplace`.`developers` SET `salary` = '7000' WHERE (`id` = '4');
-UPDATE `it_marketplace`.`developers` SET `salary` = '4000' WHERE (`id` = '5');
-UPDATE `it_marketplace`.`developers` SET `salary` = '3500' WHERE (`id` = '6');
-UPDATE `it_marketplace`.`developers` SET `salary` = '7000' WHERE (`id` = '7');
-UPDATE `it_marketplace`.`developers` SET `salary` = '1000' WHERE (`id` = '8');
+  ALTER TABLE testgoitdb.developers
+  ADD COLUMN salary int;
+  
+  UPDATE testgoitdb.developers
+  SET salary = 12000 
+  WHERE id = 1;
+  
+  UPDATE testgoitdb.developers
+  SET salary = 22000 
+  WHERE id = 2;
+  
+  UPDATE testgoitdb.developers
+  SET salary = 12000 
+  WHERE id = 3;
+  
